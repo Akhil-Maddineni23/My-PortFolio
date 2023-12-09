afdb0c1e46ffe76f8938e184d326ca627f747166
Akhil MAddineni <61157442+Akhil-Maddineni23@users.noreply.github.com>
2023-12-09 20:21:36 +0530

    Update data.js

--- client/src/Data/data.js
+++ client/src/Data/data.js
@@ -1,41 +1,30 @@
 export const skills = {
-  pl: ["C", "C++", "Python", "JavaScript", "SQL"],
+  pl: ["C", "C++", "Python", "JavaScript", "SQL", "Typescript"],
   fl: [
-    "React.js",
-    "Redux",
-    "Node.js",
-    "Express.js",
-    "MongoDB",
-    "Mongoose",
-    "JQuery",
-    "GraphQl Api",
-    "RESTful Api",
-    "GraphQl.js",
-    "Flask",
-    "Bootstrap",
-    "Pandas",
-    "Matplotlib",
-    "Beatutiful Soup",
-    "OpenCV",
+    "Data Structures and Algorithms", 
+    "Design and Analysis of Algorithms", 
+    "OOPs", 
+    "Operating Systems concepts",
+    "Monolithic Architecture",
+    "Microservices Architecture",
+    "Image Processing", 
+    "Computer Vision"
   ],
   misl: [
-    "Data Structures and Algorithms",
-    "Working with API",
-    "Computer Vision",
-    "Image Processing",
-    "Data Mining",
-    "Web Scrapping",
-    "HTML5",
-    "CSS3",
-    "Github",
-    "Git",
-    "VS Code",
-    "Linux",
-    "File Handling",
-    "Text Mining",
-    "Postman",
-    "Autocad",
-    "Fast Bowling",
+    "React", 
+    "HTML5", 
+    "CSS3", 
+    "Bootstrap", 
+    "Node", 
+    "Express",
+    "MongoDB", 
+    "Mongoose", 
+    "REST API’s", 
+    "GraphQL Api",
+    "Docker", 
+    "Kubernetes", 
+    "Jest", 
+    "NATS"
   ],
 };
 
@@ -49,6 +38,16 @@ export const projects = [
     appLink : 'https://busy-lime-hedgehog-cuff.cyclic.app'
   },
   {
+    title : ' Cattle Identification using Muzzle Pattern',
+    description : `Research on the identification of cattle using their muzzle patterns. To extract muzzles from
+    the image, I worked on a number of deep learning techniques. To get rid of the noise in
+    the image, I worked on a few image processing approaches. Additionally, a great deal of
+    feature extraction methods were developed, and they were then implemented using Python
+    and OpenCV.`,
+    tools : `Opencv, Matplotlib, Pandas , JSON`,
+    appLink : ''
+  }, 
+  {
     title : 'Password Notes App',
     description : `It is a web application for writing down and storing password notes. On the notes, any
     CRUD action may be carried out. Additionally, we have the option of sharing password
@@ -56,6 +55,22 @@ export const projects = [
     via notification every time they get a password note.`,
     tools : `MongoDB, Express, React, Node, Socket-io, HTML, CSS`,
     appLink : 'https://password-notes-app.netlify.app/'
+  },
+  {
+    title : 'Rooks Move Game',
+    description : `It's a multiplayer game. Players were connected to the server using sockets. Moving Rook from start position to 
+    target position. Alternative turns were taken by the players to reach the destination.`,
+    tools : `Phaser, Express, Socket-io`,
+    appLink : 'https://github.com/Akhil-Maddineni23/Rook-s-Move'
+  },
+  {
+    title : 'BackEnd for an tickets booking app',
+    description : `Designed backend for an tickets booking app using MicroServices. Different services were
+    created to handle the tasks of the application like Authentication, Tickets creation, Order
+    creation, Order expiration, Payment. And I have created a npm module to share the common
+    code between services.`,
+    tools : `Nodejs, TypeScript, Express, MongoDB, jest, Docker, Kubernetes, NATS`,
+    appLink : 'https://github.com/Akhil-Maddineni23/ticketing'
   },
   {
     title : 'FrontEnd - UI Challenges',
@@ -70,18 +85,9 @@ export const projects = [
     and Forecast data from Open Weather website, Cricket international teams and players from
     Cricbuzz website, Movies data from IMDB website.`,
     tools : ` React, RESTApi, JavaScript`,
-    appLink : ''
+    appLink : 'https://github.com/Akhil-Maddineni23/Data-Fetching-Using-Api-s'
   },
-  {
-    title : ' Cattle Identification using Muzzle Pattern',
-    description : `Research on the identification of cattle using their muzzle patterns. To extract muzzles from
-    the image, I worked on a number of deep learning techniques. To get rid of the noise in
-    the image, I worked on a few image processing approaches. Additionally, a great deal of
-    feature extraction methods were developed, and they were then implemented using Python
-    and OpenCV.`,
-    tools : `Opencv, Matplotlib, Pandas , JSON`,
-    appLink : ''
-  }, 
+ 
 ]
 
 export const activities = [
